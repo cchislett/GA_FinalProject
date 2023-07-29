39edc0dfad5bcfb9306ee7d1e1dc989e1f8f343a
Campbell Chislett <s3890042@student.rmit.edu.au>
2023-07-29 12:36:35 +1000

    Added API call for testing

---
+++ src/GetMovie.js
@@ -0,0 +1,38 @@
+import { API_KEY } from "./App"
+import { useEffect, useState } from 'react'
+
+function GetMovie() {
+  
+  // Placeholder user input to test API response
+  const userInput="titanic"  
+
+  // The movies returned from the initial API search
+  // These are the movies that can be used for comparison.   
+  const [movies, setMovies] = useState("")
+
+  // The user selected film from the initial search
+  // This is the movie that will be used for comparison
+  const [selectedFilm, setSelectedFilm] = useState("")
+  
+  const fetchMovies = () => {
+    fetch(`https://similar-movies.p.rapidapi.com/search?q=${userInput}`, {
+      headers: {
+        'X-RapidAPI-Host': 'similar-movies.p.rapidapi.com',
+        'X-RapidAPI-Key': API_KEY
+      },
+    })
+
+    .then(res => res.json())
+    .then(data => {console.log(data)})
+    .catch(err => { console.log(err)})
+  }
+  // Get the movie
+  useEffect(fetchMovies, []) 
+  
+  return (  
+    <div className="Movies">
+      <p>Trying to fetch movies</p>
+    </div>
+  )
+}
+export default GetMovie
